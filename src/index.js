f07eebd0e0b0f07d41203184bf26c729653c8f5a
Jenni <jenni.svensson@live.se>
2020-10-06 14:17:10 +0200

    Deleted and moved the typeDefs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import typeDefs from "./typeDefs";
-import resolvers from "./resolvers";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 const PORT = 4000 || process.env;
 const app = express();
 
 app.disable("x-powered-by");
-
-const typeDefs = gql`
-  type User {
-    name: String
-    email: String
-  }
-
-  type Query {
-    users: [User]
-  }
-`;
-
-const users = [
-  {
-    name: "test",
-    email: "test@test.com",
-  },
-  {
-    name: "test1",
-    email: "test1@test.com",
-  },
-];
-
-const resolvers = {
-  Query: {
-    users: () => users,
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
